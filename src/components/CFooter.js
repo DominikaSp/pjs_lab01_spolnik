47fce5d5d94afd366cae74131164032659d53583
DominikaSpolnik3ic2 <d.spolnik02@gmail.com>
2024-10-01 16:59:32 +0200

    Task 5.3

--- src/components/CFooter.js
+++ src/components/CFooter.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 function CFooter() {
-    return <h5>Programowanie JavaScript przykładowy serwis React.</h5>
+    let date = new Date();
+
+    return (
+
+        <div>
+            <h5>Programowanie JavaScript przykładowy serwis React. Dzisiaj mamy: {date.toString()}</h5>
+        </div>
+    )
 }
 
 export default CFooter;
