74d34325027e7363651b2ba8878c9f370957d782
DominikaSpolnik3ic2 <d.spolnik02@gmail.com>
2024-10-01 17:12:54 +0200

    Listing 5.8

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 //import logo from './logo.svg';
 import './App.css';
 import { CFooter } from './components/CFooter';
+import CHeader from './components/CHeader';
 
 function App() {
   return (
     <div className="App">
+      <CHeader />
       <CFooter />
     </div>
   );
