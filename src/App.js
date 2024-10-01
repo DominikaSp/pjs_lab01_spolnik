4983b93c1af0fe9f3eb6979b46508fa893c2ad76
DominikaSpolnik3ic2 <d.spolnik02@gmail.com>
2024-10-01 16:57:19 +0200

    Task 5.2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import CFooter from './components/CFooter';
 
 function App() {
   return (
     <div className="App">
-
+      <CFooter />
     </div>
   );
 }
---
+++ src/components/CFooter.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CFooter() {
+    return <h5>Programowanie JavaScript przykładowy serwis React.</h5>
+}
+
+export default CFooter;
